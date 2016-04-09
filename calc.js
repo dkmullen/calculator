664a90dc264426d233c1e7e5b26daff612d857e6
dkmullen <dkmullen@gmail.com>
2016-04-09 09:51:06 -0400

    docs: Add gitignore feat: Begin code for calculator refactor: Change ids and classes

--- calc.js
+++ calc.js
@@ -1,6 +1,11 @@
 /**Calc*/
+var activeInput = '';
+var activeDisplay = ko.observable(activeInput);
 
 function ViewModel() {
-	
+	this.keyListener = function(n) {
+		activeInput += n;
+		console.log(activeInput);
+	};
 }
 ko.applyBindings(new ViewModel());
