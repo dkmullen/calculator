b7381d3642445237aa11392b585a9a99025293ca
dkmullen <dkmullen@gmail.com>
2016-04-09 19:02:38 -0400

    feat: Write function for each number button

--- calc.js
+++ calc.js
@@ -1,11 +1,54 @@
 /**Calc*/
-var activeInput = '';
+var activeInput = '0';
 var activeDisplay = ko.observable(activeInput);
 
 function ViewModel() {
-	this.keyListener = function(n) {
-		activeInput += n;
-		console.log(activeInput);
+	self.enterDigit = function(n) {
+		activeDisplay(activeInput += n);
 	};
-}
+	
+	self.zero = function() {
+		self.enterDigit(0);
+	};	
+	
+	self.one = function() {
+		self.enterDigit(1);
+	};
+	
+	self.two = function() {
+		self.enterDigit(2);
+	};
+	
+	self.three = function() {
+		self.enterDigit(3);
+	};
+	
+	self.four = function() {
+		self.enterDigit(4);
+	};
+
+	self.five = function() {
+		self.enterDigit(5);
+	};
+	
+	self.six = function() {
+		self.enterDigit(6);
+	};
+	
+	self.seven = function() {
+		self.enterDigit(7);
+	};
+	
+	self.eight = function() {
+		self.enterDigit(8);
+	};
+	
+	self.nine = function() {
+		self.enterDigit(9);
+	};
+	
+	self.point = function() {
+		self.enterDigit('.');
+	};
+	}
 ko.applyBindings(new ViewModel());
