6205285f1e4a27dfc47cac3da0e9c0fcc4a62f2c
dkmullen <dkmullen@gmail.com>
2016-04-11 22:16:07 -0400

    feat: Add functions for math operands, work on top display

--- calc.js
+++ calc.js
@@ -1,16 +1,29 @@
 /**Calc*/
-var history = '';
+var topStr = '';
 var activeInput = '0';
-var topDisplay = ko.observable('');
+var topDisplay = ko.observable(topStr);
 var activeDisplay = ko.observable(activeInput);
 
 function ViewModel() {
-	self.enterDigit = function(n) {
-		if (activeInput === '0' && n !== '.') {
-			activeDisplay(activeInput = n);
+	
+	var a, b;
+	
+	self.enterDigit = function(numStr) {
+		if (activeInput === '0' && numStr !== '.') {
+			activeDisplay(activeInput = numStr);
 		} else {
-			activeDisplay(activeInput += n);
+			activeDisplay(activeInput += numStr);
 		}
+	};
+	
+	self.storeOperation = function(numStr, operandStr) {
+		if (numStr === ''){
+				topStr = topStr.slice(0, -3);
+			}
+		
+		topStr += (numStr + operandStr);
+		topDisplay(topStr);
+		activeInput = '';
 	};
 	
 	self.zero = function() {
@@ -62,12 +75,23 @@ function ViewModel() {
 	self.C = function() {
 		topDisplay('');
 		activeDisplay(activeInput = '0');
+		topStr = '';
 	};
 	
 	self.plus = function() {
-		topDisplay(activeInput + ' +');
-		activeInput = '';
-		
+		self.storeOperation(activeInput, ' + ');
+	};
+	
+	self.minus = function() {
+		self.storeOperation(activeInput, ' - ');
+	};
+	
+	self.times = function() {
+		self.storeOperation(activeInput, ' * ');
+	};
+	
+	self.divide = function() {
+		self.storeOperation(activeInput, ' / ');
 	};
 }
 ko.applyBindings(new ViewModel());
